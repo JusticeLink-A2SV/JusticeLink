6456374b30a6d5d481f8a8c8f24dd6dd46dc25b4
Bz14 <bezueyerusalem@gmail.com>
2024-07-15 12:27:01 +0300

    Context provider updated.

---
+++ JusiceLink/src/Context/ContextProvider.jsx
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+import PropTypes from "prop-types";
+import data from "../Assets/dummy_data.json";
+
+const LawyerContext = createContext();
+
+const LawyerProvider = ({ children }) => {
+  return (
+    <LawyerContext.Provider value={data}>{children}</LawyerContext.Provider>
+  );
+};
+
+LawyerProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export { LawyerContext, LawyerProvider };
